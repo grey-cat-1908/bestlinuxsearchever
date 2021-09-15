0a1c5fedcc8fc33d818a8470e20c293f88044074
Victor Kotlin <grey.cat1908@gmail.com>
2021-09-15 15:01:48 +0300

    upload the best project in the world

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+    <v-footer style="background-color: green;">
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>ООО БРАВЛСУПЕРУЛЬТРАМЕГАБОКСВЫБИВАНИЕЮТУБТВИЧВЗЛОМЧИТЫ</strong>
+      </v-card-text>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  data: () => ({
+    //
+  }),
+};
+</script>
